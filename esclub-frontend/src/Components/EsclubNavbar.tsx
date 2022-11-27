f5d492fba7898567ac42b7b78296a740a77f9f2d
Bengi Su Bilgin <bengisuubilginn@gmail.com>
2022-11-27 22:46:24 +0300

    home updated

--- esclub-frontend/src/Components/EsclubNavbar.tsx
+++ esclub-frontend/src/Components/EsclubNavbar.tsx
@@ -4,12 +4,13 @@ import Navbar from "react-bootstrap/Navbar";
 function EsclubNavbar() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar style={{ background: "#4E4E4E"}} variant="dark">
         <Container>
           <Nav className="ms-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="clubs">Clubs</Nav.Link>
-            <Nav.Link href="events">Events</Nav.Link>
+            <Nav.Link href="home" style={{color:"white"}}>
+             Home</Nav.Link>
+            <Nav.Link href="clubs" style={{color:"white"}}>Clubs</Nav.Link>
+            <Nav.Link href="events" style={{color:"white"}}>Events</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
--- esclub-frontend/src/constant/announceData.ts
+++ esclub-frontend/src/constant/announceData.ts
@@ -1,43 +1,27 @@
 export const announces =[
     {
+        src:"https://ogrencikariyeri.com//AdminFiles/CMS/Content/wp-content/uploads/2018/01/Ba%C5%9Fl%C4%B1ks%C4%B1z-1.png",
         name:"EESTEC",
         fullName:"Electrical Engineering STudents' European assoCiation",
         announce:"2022 Kayıt alımları yarın sona eriyor!!!"
     },
     {
+        src:"https://yt3.ggpht.com/ytc/AMLnZu_90sJKQFD2r7nwsL3tcfV9PQu_Yqyjpmvsn9d5=s900-c-k-c0x00ffffff-no-rj",
         name:"Evk",
-        fullName:"Electrical Engineering STudents' European assoCiation",
-        announce:"2022 Kayıt alımları yarın sona eriyor!!!"
+        fullName:"Endüstri ve Verimlilik Kulübü",
+        announce:"Start Up 22 çok yakında!!!"
     },
     {
+        src:"https://ogrencikariyeri.com//AdminFiles/CMS/Content/wp-content/uploads/2018/01/Ba%C5%9Fl%C4%B1ks%C4%B1z-1.png",
         name:"EESTEC",
         fullName:"Electrical Engineering STudents' European assoCiation",
         announce:"2022 Kayıt alımları yarın sona eriyor!!!"
     },
     {
-        name:"EESTEC",
-        fullName:"Electrical Engineering STudents' European assoCiation",
-        announce:"2022 Kayıt alımları yarın sona eriyor!!!"
-    },
-    {
-        name:"EESTEC",
-        fullName:"Electrical Engineering STudents' European assoCiation",
-        announce:"2022 Kayıt alımları yarın sona eriyor!!!"
-    },
-    {
-        name:"EESTEC",
-        fullName:"Electrical Engineering STudents' European assoCiation",
-        announce:"2022 Kayıt alımları yarın sona eriyor!!!"
-    },
-    {
-        name:"EESTEC",
-        fullName:"Electrical Engineering STudents' European assoCiation",
-        announce:"2022 Kayıt alımları yarın sona eriyor!!!"
-    },
-    {
-        name:"EESTEC",
-        fullName:"Electrical Engineering STudents' European assoCiation",
-        announce:"2022 Kayıt alımları yarın sona eriyor!!!"
+        src:"https://yt3.ggpht.com/ytc/AMLnZu_90sJKQFD2r7nwsL3tcfV9PQu_Yqyjpmvsn9d5=s900-c-k-c0x00ffffff-no-rj",
+        name:"Evk",
+        fullName:"Endüstri ve Verimlilik Kulübü",
+        announce:"Start Up 22 çok yakında!!!"
     },
 ]
 
