533a001678e3799dbe0df9f9991bc3c0ad83e083
sezinacet <sezinacet@eskisehir.edu.tr>
2023-01-01 18:38:57 +0300

    UI design

--- esclub-frontend/src/Components/Clubs/EsclubFooter.tsx
+++ esclub-frontend/src/Components/Clubs/EsclubFooter.tsx
@@ -1,25 +1,19 @@
 import * as React from "react";
+import { Container, Navbar } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export class EsclubFooter extends React.Component {
   render() {
     return (
-      <footer style={{ backgroundColor: "maroon", marginTop: "100px" }}>
-        <div className="container-fluid footer">
-          <div className="row">
-            <div className="col-md-12">
-              <ul className="pull-right list-unstyled">
-                <li
-                  className="text-center"
-                  style={{ marginTop: "15px", color: "white" }}
-                >
-                  © Microsoft {new Date().getFullYear()} Copyright: EsClub
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <>
+        <Navbar bg="danger" expand="lg" style={{ marginTop: "30px" }}>
+          <Nav className="me-auto" style={{ margin: "auto" }}>
+            {" "}
+            © Microsoft {new Date().getFullYear()} Copyright: EsClub
+          </Nav>
+        </Navbar>
+      </>
     );
   }
 }
